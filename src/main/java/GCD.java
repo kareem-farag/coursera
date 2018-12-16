81aee43c830b4840036d7f8bccc9629616eafa82
Kareem-Farag <kareemfarag@hotmail.com>
2018-12-16 23:39:44 +0200

    Building Greatest Common Divisor method , having an error related to missing return statement

---
+++ src/main/java/GCD.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int firstInt = in.nextInt();
+        int secondInt = in.nextInt();
+
+
+        System.out.println(getGCD(firstInt, secondInt));
+
+    }
+
+    public static int getGCD(int firstInt, int secondInt) {
+
+        double result = (Double.valueOf(firstInt) / Double.valueOf(secondInt));
+
+        result = (result - Math.floor(result)) * secondInt;
+
+
+        firstInt = secondInt;
+        secondInt = (int) Math.round(result);
+        System.out.println(firstInt + "-" + secondInt);
+        if (secondInt == 0) {
+            return firstInt;
+        } else {
+            getGCD(firstInt, secondInt);
+
+        }
+        return firstInt;
+// 1653264 - 3918848
+
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
         int n = in.nextInt();
 
 
+
         System.out.println(getFib(n));
 
     }
